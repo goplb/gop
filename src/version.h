3b980e8de827599b11db557d85a419dfd853a02b
goplb <goplborg@hotmail.com>
2018-10-15 07:39:33 -0400

    * Open source code.

---
+++ src/version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern const char* const GOPLB_VERSION_TAG;
+extern const char* const GOPLB_VERSION;
+extern const char* const GOPLB_RELEASE_NAME;
+extern const char* const GOPLB_VERSION_FULL;
